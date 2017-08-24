6141d37fb706900a157c88b0066e84a899ae1c97
johnrb2 <j.r.bench.ii@gmail.com>
2017-08-24 15:01:33 -0600

    minor changes to make use of es6 syntax for functions in api.js

--- api.js
+++ api.js
@@ -1,34 +1,31 @@
 /*
  * Copyright 2017 Brigham Young University
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
+ * Licensed under the Apache License, Version 2.0 (the 'License');
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
+ * distributed under the License is distributed on an 'AS IS' BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
  */
-"use strict";
 
-const SansServer        = require('sans-server');
+const SansServer = require('sans-server');
 const SansServerSwagger = require('sans-server-swagger');
-const byuJwt            = require('byu-jwt');
+const byuJwt = require('byu-jwt');
 
 byuJwt.cacheWellknowns = true;
 const wellknown = 'https://api.byu.edu/.well-known/openid-configuration';
 
-// create a sans-server instance and export it
 const api = SansServer();
-module.exports = api;
 
 api.hook(function defaultResponseHandler(state) {
-  if(state.body instanceof Error && state.body.status) {
+  if (state.body instanceof Error && state.body.status) {
     this.body({
       return_code: state.body.status,
       explanation: state.body.message,
@@ -37,20 +34,21 @@ api.hook(function defaultResponseHandler(state) {
   }
 });
 
-api.use(function (req, res, next) {
-  if (req.path === "/") {
+api.use(async (req, res, next) => {
+  if (req.path === '/') {
     next();
   }
   else {
-    byuJwt.authenticate(req.headers, wellknown)
-      .then(function (verifiedJWTs) {
-        req.verifiedJWTs = verifiedJWTs;
-        next();
-      })
-      .catch(result => res.status(403).send({
-        "return_code": 403,
-        "error": "JWT Invalid"
-      }));
+    try {
+      req.verifiedJWTs = await byuJwt.authenticate(req.headers, wellknown);
+      next();
+    } catch (error) {
+      console.error(error.stack);
+      res.status(403).send({
+        'return_code': 403,
+        'error': 'JWT Invalid'
+      });
+    }
   }
 });
 
@@ -59,11 +57,13 @@ api.use(SansServerSwagger({
   development: true,
   logs: 'verbose',
   swagger: './swagger.json',
-  exception: function exception(res, state) {
-    res.body({
-      return_code: state.statusCode,
-      explanation: state.body,
-      error: state.body
-    });
-  }
+  // exception: function exception(res, state) {
+  //   res.body({
+  //     return_code: state.statusCode,
+  //     explanation: state.body,
+  //     error: state.body
+  //   });
+  // }
 }));
+
+module.exports = api;
